6cf31f4dc0f6f5ec731f77ba194b3ce44540a926
s15004 <s15004@std.it-college.ac.jp>
2016-07-08 10:35:05 +0900

    test

---
+++ 10001.py
@@ -0,0 +1,20 @@
+1
+def check_sum_2time_over_60(part1, part2):
+    if part1 + part2 >= 60:
+        print("OVER")
+    else:
+        print("NO")
+
+
+2
+def tux_include(cost):
+    if num * 1.08 >= 5000:
+        print("OVER")
+    else:
+        print("NO")
+
+
+3
+
+def judge_rank(score):
+
