21b219904c6629e05136b2adf1f6fdb5a7b825b8
s15004 <s15004@std.it-college.ac.jp>
2016-07-08 10:10:10 +0900

    Initial commit

---
+++ basic.py
@@ -0,0 +1,30 @@
+#1
+def print_self_information():
+    print ("ITスペシャリスト")
+    print ("2年")
+    print ("s15004")
+
+
+#2
+def print_how_many_years_to_80():
+    yongage = 20
+    oldage = 80
+    print(oldage - yongage )
+
+#3与えられた整数が偶数か奇数か
+def print_add_or_even():
+    if num / 2 == 0:
+        print("偶数")
+    else:
+        print("奇数")
+
+
+
+
+
+
+
+#4
+def print_hoge():
+    from random import randint
+
